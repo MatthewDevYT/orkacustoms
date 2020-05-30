4d8f3f42ffca6b92475acc5c2d02fa06483d07a9
MatthewDevYT <62351071+MatthewDevYT@users.noreply.github.com>
2020-05-30 10:09:22 +0200

    init
    init

--- commands/tcustoms.js
+++ commands/tcustoms.js
@@ -26,7 +26,6 @@ module.exports.run = async(client, message, args) => {
     }
 
     var tcustomEmbed = new discord.MessageEmbed()
-        .setTitle("@everyone")
         .setTitle("Trio Custom")
         .setColor("#00ea00")
         .setDescription(`Code: ${options.code} \n Hosted By ${message.author} \n Mode: Trio Arena`)
